f26e24081b0444f145bd32d078cde9c9ac3a46eb
Aurélien Cros <aure.cros@gmail.com>
2023-08-07 15:01:01 +0200

    feat/main : Losses
    Added a chart of total losses per fighter

--- components/HallOfFame.js
+++ components/HallOfFame.js
@@ -9,8 +9,13 @@ export default class HallOfFame {
         this.container = root;
 
         this.createModal();
+        this.chartContainer = this.element.querySelector("#chart");
         this.initCloseBtn();
-        this.createChart();
+        this.initView();
+    }
+    async initView() {
+        await this.createWinsChart();
+        this.initLinks();
     }
     createModal() {
         const template = {
@@ -20,10 +25,63 @@ export default class HallOfFame {
             },
             children: [
                 {
-                    tag: "canvas",
+                    tag: "div",
+                    children: [
+                        {
+                            tag: "h2",
+                            content: "Hall of Fame",
+                            attrs: {
+                                class: "text-center"
+                            }
+                        },
+                        {
+                            tag: "p",
+                            content: "and statistics",
+                            attrs: {
+                                class: "text-center"
+                            }
+                        },
+                    ]
+                },
+                {
+                    tag: "nav",
                     attrs: {
-                        id: "wins-chart"
-                    }
+                        class: "d-flex gap-3 justify-content-center"
+                    },
+                    children: [
+                        {
+                            tag: "a",
+                            content: "Wins",
+                            attrs: {
+                                href: "#",
+                                class: "link-to-ws"
+                            }
+                        },
+                        {
+                            tag: "a",
+                            content: "Losses",
+                            attrs: {
+                                href: "#",
+                                class: "link-to-ls"
+                            }
+                        }
+                    ]
+                }
+                ,
+                {
+                    tag: "div",
+                    attrs: {
+                        class: "w-100",
+                        style: "height: 400px"
+                    },
+                    children: [
+                        {
+                            tag: "canvas",
+                            attrs: {
+                                id: "chart"
+                            }
+                        }
+                    ]
                 },
                 {
                     tag: "button",
@@ -40,7 +98,18 @@ export default class HallOfFame {
             this.element.classList.add('show');
         }, 5);
     }
-
+    initLinks() {
+        this.element.querySelector(".link-to-ws").addEventListener("click", (e) => {
+            e.preventDefault();
+            this.clearChart();
+            this.createWinsChart();
+        })
+        this.element.querySelector(".link-to-ls").addEventListener("click", (e) => {
+            e.preventDefault();
+            this.clearChart();
+            this.createLossesChart();
+        })
+    }
     initCloseBtn() {
         this.closeBtn = this.element.querySelector(".modal-close");
         this.closeBtn.addEventListener("click", () => {
@@ -51,23 +120,35 @@ export default class HallOfFame {
         })
     }
 
-    async getData() {
-        const wins = await fetch('./api/v1/fights/number-of-wins')
-            .then(response => response.json())
-            .then(data => data);
+    async getData(type) {
+        switch (type) {
 
-        const data = {
-            names: wins.map((rec) => rec.name),
-            wins: wins.map((rec) => rec.wins),
+            case 'wins':
+                const wins = await fetch('./api/v1/fights/number-of-wins')
+                    .then(response => response.json())
+                    .then(data => data);
+
+                const dataW = {
+                    names: wins.map((rec) => rec.name),
+                    wins: wins.map((rec) => rec.wins),
+                }
+                return dataW;
+
+            case 'losses':
+                const losses = await fetch('./api/v1/fights/number-of-losses')
+                    .then(response => response.json())
+                    .then(data => data);
+
+                const dataL = {
+                    names: losses.map((rec) => rec.name),
+                    losses: losses.map((rec) => rec.losses),
+                }
+                return dataL;
         }
-        console.log(wins, data);
-        return data;
     }
-    async createChart() {
-        this.chart = this.element.querySelector("#wins-chart");
-        const data = await this.getData();
-        console.log(data);
-        new Chart(this.chart, {
+    async createWinsChart() {
+        const data = await this.getData('wins');
+        this.chart = new Chart(this.chartContainer, {
             type: 'bar',
             data: {
                 labels: data.names,
@@ -80,15 +161,43 @@ export default class HallOfFame {
             options: {
                 scales: {
                     y: {
-                        beginAtZero: true
-                    }
-                },
-                plugins: {
-                    decimation: {
-                        enabled: false
+                        beginAtZero: true,
+                        ticks: {
+                            precision: 0
+                        }
                     }
                 }
             }
         });
+        return true;
+    }
+    async createLossesChart() {
+        const data = await this.getData('losses');
+
+        this.chart = new Chart(this.chartContainer, {
+            type: 'bar',
+            data: {
+                labels: data.names,
+                datasets: [{
+                    label: '# of losses',
+                    data: data.losses,
+                    borderWidth: 1,
+                    backgroundColor: '#FF9191'
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        ticks: {
+                            precision: 0
+                        }
+                    }
+                }
+            }
+        });
+    }
+    clearChart() {
+        this.chart.destroy();
     }
 }
